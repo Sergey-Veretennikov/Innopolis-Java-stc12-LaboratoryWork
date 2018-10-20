07eab790d533385dee06a07762be6eb811e6f43d
Sergey Veretennikov <oz-15@mail.ru>
2018-10-20 12:43:59 +0400

    Craete Test ResourceHandlerTest.java + ResourceReaderTest.java

--- src/test/java/ru/innopolis/lw02/ResourceHandlerTest.java
+++ src/test/java/ru/innopolis/lw02/ResourceHandlerTest.java
@@ -10,7 +10,6 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ResourceHandlerTest {
@@ -31,7 +30,7 @@ class ResourceHandlerTest {
     }
 
 
-    @Test
+/*    @Test
     void checksSizeOfResourcesBlockingQueueTest() {
         resourceReader.run();
 
@@ -44,7 +43,7 @@ class ResourceHandlerTest {
         assertEquals(0, resourcesBlockingQueue.size());
         LOGGER.warn(resourcesBlockingQueue.toString());
         LOGGER.warn(stringResultBlockingQueue.toString());
-    }
+    }*/
 
     @Test
     void checksNoEmptyOfBlockingQueueTest() {
